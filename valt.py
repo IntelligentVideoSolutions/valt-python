3eaa61c5b1d849b221cb80a68e307e358dfcc9ae
jkbuttitta <jbuttitta@ipivs.com>
2025-08-18 10:02:29 -0500

    Added get_camera_presets, create_camera_presets, apply_camera_presets, and delete_camata_presets

--- valt.py
+++ valt.py
@@ -52,7 +52,7 @@ class VALT:
 			self.selected_room = None
 
 		if 'room_check_interval' in kwargs:
-			self.room_check_interval = int(kwargs['room'])
+			self.room_check_interval = int(kwargs['room_check_interval'])
 		else:
 			self.room_check_interval = 5
 
@@ -686,8 +686,10 @@ class VALT:
 		self._errormsg = newmsg
 		for callback in self._errormsg_observers:
 			callback(self._errormsg)
-	def bind_to_errormg(self,callback):
+	def bind_to_errormsg(self,callback):
 		self._errormsg_observers.append(callback)
+	def bind_to_errormg(self, callback):
+		self.bind_to_errormsg(callback)
 	def disconnect(self):
 		self.kill_threads = True
 
@@ -840,3 +842,71 @@ class VALT:
 			self.handleerror(e)
 			return False
 
+	def get_camera_presets(self, camera_id):
+		# Function to retrieve presets for a given camera.
+		# Returns a list of preset dicts or 0 on failure.
+		if self.accesstoken == 0:
+			self.logger.error(__name__ + ": Not Currently Authenticated to VALT")
+			return 0
+
+		url = self.baseurl + f'presets/{camera_id}?access_token=' + self.accesstoken
+		data = self.send_to_valt(url)
+		if isinstance(data, list):
+			return data
+		else:
+			self.handleerror("Unable to get camera presets")
+			return 0
+
+
+	def create_camera_preset(self, camera_id, name):
+		# Function to create a new preset for a given camera.
+		# Returns preset ID or 0 on failure.
+		if self.accesstoken == 0:
+			self.logger.error(__name__ + ": Not Currently Authenticated to VALT")
+			return 0
+
+		url = self.baseurl + f'presets/{camera_id}/add?access_token=' + self.accesstoken
+		values = {"name": name}
+		data = self.send_to_valt(url, values=values)
+		if isinstance(data, dict) and 'id' in data:
+			self.logger.info(__name__ + f": Preset '{name}' created for camera {camera_id}")
+			return data['id']
+		else:
+			self.handleerror("Unable to create preset")
+			return 0
+
+
+	def apply_camera_preset(self, camera_id, preset_id):
+		# Function to apply an existing preset to a given camera.
+		# Returns preset ID or 0 on failure.
+		if self.accesstoken == 0:
+			self.logger.error(__name__ + ": Not Currently Authenticated to VALT")
+			return 0
+
+		url = self.baseurl + f'presets/{camera_id}/apply?access_token=' + self.accesstoken
+		values = {"preset": preset_id}
+		data = self.send_to_valt(url, values=values)
+		if isinstance(data, dict) and 'id' in data:
+			self.logger.info(__name__ + f": Preset {preset_id} applied to camera {camera_id}")
+			return data['id']
+		else:
+			self.handleerror("Unable to apply preset")
+			return 0
+
+
+	def delete_camera_preset(self, camera_id, preset_id):
+		# Function to delete an existing preset from a given camera.
+		# Returns deleted preset ID or 0 on failure.
+		if self.accesstoken == 0:
+			self.logger.error(__name__ + ": Not Currently Authenticated to VALT")
+			return 0
+
+		url = self.baseurl + f'presets/{camera_id}/delete?access_token=' + self.accesstoken
+		values = {"preset": preset_id}
+		data = self.send_to_valt(url, values=values)
+		if isinstance(data, dict) and 'id' in data:
+			self.logger.info(__name__ + f": Preset {preset_id} deleted from camera {camera_id}")
+			return data['id']
+		else:
+			self.handleerror("Unable to delete preset")
+			return 0
